abc083bc46da970361bfe8e936175d24d709603d
Rostam Mahabadi <Rostammahabadi@gmail.com>
2020-05-22 09:49:47 -0600

    add test for presentation

--- spec/features/studio/studio_index_spec.rb
+++ spec/features/studio/studio_index_spec.rb
@@ -4,9 +4,11 @@ RSpec.describe "In the studio index page" do
   it "shows a list of all movie studios and i see a list of all movies" do
     studio1 = Studio.create(name: "Awesome", location: "Evergreen")
     movie_1 = studio1.movies.create(title: "Afflec", creation_year: 2007, genre: "Action")
+    movie_2 = studio1.movies.create(title: "movie", creation_year: 2005, genre: "Horror")
     movie_1.actors.create(name: "Ben Afflec", age: 32)
-    
+
     visit "/studios"
-    expect(page).to have_content("")
+    expect(page).to have_content("#{movie_1.title}")
+    expect(page).to have_content("#{movie_2.title}")
   end
 end
